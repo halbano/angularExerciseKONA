e2c749a0652d06529a65328f1b623ed57702f9b0
Hernán Albano <hernanalbano@gmail.com>
2017-08-31 13:48:27 -0300

    Added HELP with forms and users list, removed employees, added users

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module('MobileAngularUiExamples', [
+var app = angular.module('IBECFinal', [
     "ngRoute",
     "ngTouch",
     "mobile-angular-ui"
@@ -14,6 +14,17 @@ app.config(function ($routeProvider) {
     $routeProvider.when('/', { 
             templateUrl: "templates/listemployee.html",
             controller: "MainController"
-        });
+    });
+
+    $routeProvider.when('/users/new', { 
+            templateUrl: "templates/usersFormView.html",
+            controller: "MainController"
+    });
+
+    $routeProvider.when('/users/:id/edit', { 
+            templateUrl: "templates/usersFormView.html",
+            controller: "MainController"
+    });
+
     $routeProvider.when('/about', {templateUrl: "templates/about.html"});
 });
